f15f01d46717a0622d2ed30ac8989d50bb0bd4f1
Mario Mip <mariomip98@gmail.com>
2025-04-25 13:39:23 -0600

    Fixed errors and added Mark unmark btn, also btn to delete tasks and added CSS

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -9,7 +10,11 @@ def home():
 	if request.method == 'POST':
 		title = request.form['title']
 		description = request.form.get('description')
-		due_date = request.form.get('due_date')
+
+		due_date_str = request.form.get('due_date')
+		due_date = None
+		if due_date_str:
+			due_date = datetime.strptime(due_date_str, '%Y-%m-%dT%H:%M')
 
 		task = Task(
 			title=title,
@@ -17,6 +22,8 @@ def home():
 			due_date=due_date if due_date else None,
 			user_id=1 # Fixed for now
 		)
+
+		
 		db.session.add(task)
 		db.session.commit()
 		return redirect(url_for('main.home'))
@@ -24,3 +31,17 @@ def home():
 
 	tasks = Task.query.filter_by(user_id=1).order_by(Task.created_at).all()
 	return render_template('home.html', tasks=tasks)
+
+@main.route('/toggle_done/<int:task_id>', methods=['POST'])
+def toggle_done(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.is_done = not task.is_done
+    db.session.commit()
+    return redirect(url_for('main.home'))
+
+@main.route('/delete_task/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('main.home'))
