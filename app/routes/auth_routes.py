992889545ea7d99118fb3c098215d7195d00e01d
Mario Mip <mariomip98@gmail.com>
2025-04-25 21:08:58 -0600

    Made a ton of changes -Made auth_routes.py work with current user -Modified CSS to look nice -register.html changed to auth folder -Made base.html some divs to change view of btns -Made home.html animate Scripts, added backdrop,  function to show new tasks as a floating modal,  some more stuff to make modal look nice

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -23,7 +23,7 @@ def register():
         new_user = User(
             username=username,
             email=email,
-            password=generate_password_hash(password, method='sha256')
+            password=generate_password_hash(password, method='pbkdf2:sha256')
         )
         db.session.add(new_user)
         db.session.commit()
